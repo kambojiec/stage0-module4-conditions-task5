699c7a37c8f26714a22672f5f28fe25017f33588
Vlad Kupchik <vladkupchik15@gmail.com>
2023-07-04 03:24:08 +0300

    j

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,34 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeap;
+        switch (year % 4) {
+            case 0:
+                switch (year % 100) {
+                    case 0:
+                        switch (year % 400) {
+                            case 0:
+                                isLeap = true;
+                                break;
+                            default:
+                                isLeap = false;
+                                break;
+                        }
+                        break;
+                    default:
+                        isLeap = true;
+                        break;
+                }
+                break;
+            default:
+                isLeap = false;
+                break;
+        }
+
+        if (isLeap) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
     }
 }
